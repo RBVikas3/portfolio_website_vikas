7c34b0d54bf5fb7b4dbf4861b8b45879f1e04dbc
vikas <vikasrb24@gmail.com>
2024-03-09 12:40:09 +0530

    bohb

--- src/App.jsx
+++ src/App.jsx
@@ -69,8 +69,7 @@ function App() {
               </div>
               <div>
                 <p className="mt-4 text-gray-400">
-                Highly motivated Front-end developer 1.2 years with a passion for crafting engaging UIs and backend architecture. Built and deployed 30 websites (personal + professional), proficient in React, Next.js, MongoDB, Node.js, jQuery, AWS, payment integrations (RezorPay, Stripe). Lead a 5-member team, skilled in communication and design. Eager to leverage my expertise for your team's success!
-                </p>
+                Highly motivated Front-end developer 1.2 years with a passion for crafting engaging UIs and backend architecture. Built and deployed 30 websites (personal + professional), proficient in React, Next.js, MongoDB, Node.js, jQuery, AWS, payment integrations (RezorPay, Stripe). </p>
                 <button className="px-8 shadow-gray-500 shadow-md py-5 mt-5 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
                   Download resume
                 </button>
