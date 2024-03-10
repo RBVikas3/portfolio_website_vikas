03edbcf428babd0994434ef5ab950278b5e4e61e
vikas <vikasrb24@gmail.com>
2024-03-11 02:00:25 +0530

    added some needede information

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Porfile from "./assets/Photo.png";
+import Porfile from "./assets/Photo.jpeg";
 import Project1 from "./assets/project1.png";
 import Project2 from "./assets/project2.png";
 import Project3 from "./assets/project3.png";
@@ -9,6 +9,7 @@ import LinkedIn from "./assets/linkedin.svg";
 import Instagram from "./assets/instagram.svg";
 import ArrowDown from "./assets/arrow-down.svg";
 import { useEffect, useState } from "react";
+import Resume from "./assets/resume.pdf"
 
 function App() {
   const [scrolling, setScrolling] = useState(false);
@@ -29,16 +30,71 @@ function App() {
   }, [])
   const openLiveProject = () => {
     const liveProjectUrl = 'https://skin-protection-cream.vercel.app';
+  
+    // Open the live project URL in a new tab
+    window.open(liveProjectUrl, '_blank');
+  };
+  const openLiveProjectshop = ()=>{
+    const liveProjectUrl = 'https://dashboard-starter-theta.vercel.app/';
 
     // Navigate to the live project URL in the current tab
-    window.location.href = liveProjectUrl;
-  };
+    window.open(liveProjectUrl, '_blank');
+
+  }
   const openGithubRepository = () => {
     const liveProjectUrl = 'https://github.com/RBVikas3/Skin-Protection-Cream';
 
     window.open(liveProjectUrl, '_blank');
   };
-  
+  const openGithubRepositoryShop = () => {
+    const liveProjectUrl = 'https://github.com/RBVikas3/Dashboard_Starter';
+
+    window.open(liveProjectUrl, '_blank');
+  };
+  const handleDownloadResume = () => {
+    // Use the public folder for the file path
+    const resumeFilePath = Resume;
+
+    // Create a link element
+    const link = document.createElement('a');
+
+    // Set the href attribute to the file path
+    link.href = resumeFilePath;
+
+    // Set the download attribute to specify the file name
+    link.download = 'resume.pdf';
+
+    // Append the link to the body
+    document.body.appendChild(link);
+
+    // Trigger a click on the link to start the download
+    link.click();
+
+    // Remove the link from the body
+    document.body.removeChild(link);
+  };
+  const openLiveProjectGPT_3 = ()=>{
+    const liveProjectUrl = 'https://gpt-3-jsm-lime.vercel.app/';
+
+    window.open(liveProjectUrl, '_blank');
+
+  }
+  const openGithubRepositoryGPT_3 = ()=>{
+    const liveProjectUrl = 'https://github.com/RBVikas3/GPT3_JSM';
+
+    window.open(liveProjectUrl, '_blank');
+    
+  }
+  const  openLiveProjectresWeb = ()=>{
+    const liveProjectUrl = 'https://restaurant-website-opal-two.vercel.app/';
+
+    window.open(liveProjectUrl, '_blank');
+  }
+  const openGithubRepositoryresWeb = ()=>{
+    const liveProjectUrl = 'https://github.com/RBVikas3/Restaurant_website/';
+
+    window.open(liveProjectUrl, '_blank');
+  }
 
   return (
     <div className="max-w-4xl m-auto relative">
@@ -81,15 +137,15 @@ function App() {
               </div>
               <div>
                 <p className="mt-4 text-gray-400">
-                Highly motivated Front-end developer 1.2 years with a passion for crafting engaging UIs and backend architecture. Built and deployed 30 websites (personal + professional), proficient in React, Next.js, MongoDB, Node.js, jQuery, AWS, payment integrations (RezorPay, Stripe). </p>
-                <button className="px-8 shadow-gray-500 shadow-md py-5 mt-5 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                Highly motivated Front-end developer 1.2 years with a passion for crafting engaging UIs and backend architecture. Built  30 websites (personal + professional), proficient in React, Next.js, MongoDB, Node.js, jQuery, AWS, payment integrations (RezorPay, Stripe). </p>
+                <button onClick={handleDownloadResume} className="px-8 shadow-gray-500 shadow-md py-5 mt-5 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
                   Download resume
                 </button>
               </div>
             </div>
             <div className="relative">
               <div className="after:bg-[url('../large-long.png')] after:bg-contain after:block after:bg-no-repeat after:w-[420px] after:h-[320px] after:absolute after:top-0 after:-left-20 sm:after:-left-40 before:bg-[url('../small.png')] before:bg-contain before:block before:bg-no-repeat before:w-[220px] before:h-[220px] before:absolute before:bottom-0 before:-right-10">
-                <img src={Porfile} className="relative z-10 w-[280px] m-auto sm:w-[600px]" />
+                <img src={Porfile} className="relative z-10 w-[280px] m-auto sm:w-[600px] rounded-full" />
               </div>
             </div>
           </div>
@@ -102,17 +158,16 @@ function App() {
               <div className="border border-gray-500 rounded-md p-5 flex-1">
                 <img src={Project1} className="w-full h-auto" />
                 <h3 className="text-2xl font-semibold mt-8">
-                  Furniture store landing page
+                 Shop Dashboard
                 </h3>
                 <p className="text-gray-400 text-sm mt-2">
-                  Responsive HTML/CSS layout for online furniture store. HTML5,
-                  CSS3 (SCSS)
+                Responsive React application for an online store of Shop Dashboard. Built with HTML5, CSS3 (SCSS), and React.
                 </p>
                 <div className="flex mt-12 gap-2">
-                  <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                  <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700" onClick={openLiveProjectshop}>
                     Live preview
                   </button>
-                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
+                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500"onClick={openGithubRepositoryShop}>
                     Checkout github
                   </button>
                 </div>
@@ -139,17 +194,16 @@ function App() {
               <div className="border border-gray-500 rounded-md p-5 flex-1">
                 <img src={Project3} className="w-full h-auto" />
                 <h3 className="text-2xl font-semibold mt-8">
-                  Landing page for front-end developer
+                 GPT_3
                 </h3>
                 <p className="text-gray-400 text-sm mt-2">
-                  Responsive HTML/CSS layout for landing page for front-end
-                  developer. HTML5, CSS3 (SCSS)
+                Responsive React application for an online store of GPT_3. Built with HTML5, CSS3 (SCSS), and React.
                 </p>
                 <div className="flex gap-4 mt-12">
-                  <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                  <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700"  onClick={openLiveProjectGPT_3}>
                     Live preview
                   </button>
-                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
+                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500"  onClick={openGithubRepositoryGPT_3}>
                     Checkout github
                   </button>
                 </div>
@@ -157,17 +211,16 @@ function App() {
               <div className="border border-gray-500 rounded-md p-5 flex-1">
                 <img src={Project4} className="w-full h-auto" />
                 <h3 className="text-2xl font-semibold mt-8">
-                  Website redesign for The Venus project
+                Restaurant Website
                 </h3>
                 <p className="text-gray-400 text-sm mt-2">
-                  Responsive HTML/CSS layout for The Venus project. HTML5, CSS3
-                  (SCSS)
+                Responsive React application for an online store of Restaurant Website. Built with HTML5, CSS3 (SCSS), and React.
                 </p>
                 <div className="flex gap-2 mt-12">
-                  <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                  <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700"  onClick={openLiveProjectresWeb}>
                     Live preview
                   </button>
-                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
+                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500" onClick={openGithubRepositoryresWeb}>
                     Checkout github
                   </button>
                 </div>
@@ -189,7 +242,7 @@ function App() {
               </div>
               <div className="mt-8">
                 <div className="flex justify-between items-center">
-                  <h2 className="font-semibold">CSS, Sass & Bootstrap</h2>
+                  <h2 className="font-semibold">CSS, Sass , Bootstrap ,Tailwind CSS And Material UI </h2>
                   <p className="text-gray-500">Advanced</p>
                 </div>
                 <span className="w-full h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
@@ -203,34 +256,22 @@ function App() {
                 </div>
                 <span className="w-full h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
               </div>
+             
               <div className="mt-8">
                 <div className="flex justify-between items-center">
-                  <h2 className="font-semibold">UI design in Figma</h2>
+                  <h2 className="font-semibold">React And Next JS</h2>
                   <p className="text-gray-500">Advanced</p>
                 </div>
-                <span className="w-[75%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+                <span className="w-[100%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
               </div>
               <div className="mt-8">
                 <div className="flex justify-between items-center">
-                  <h2 className="font-semibold">Angular</h2>
-                  <p className="text-gray-500">Advanced</p>
+                  <h2 className="font-semibold">Node js And MongoDB</h2>
+                  <p className="text-gray-500">Intermediate</p>
                 </div>
-                <span className="w-[50%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+                <span className="w-[70%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
               </div>
-              <div className="mt-8">
-                <div className="flex justify-between items-center">
-                  <h2 className="font-semibold">React</h2>
-                  <p className="text-gray-500">Advanced</p>
-                </div>
-                <span className="w-[90%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
-              </div>
-              <div className="mt-8">
-                <div className="flex justify-between items-center">
-                  <h2 className="font-semibold">React Native</h2>
-                  <p className="text-gray-500">Advanced</p>
-                </div>
-                <span className="w-[45%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
-              </div>
+             
             </div>
           </div>
         </section>
@@ -243,82 +284,92 @@ function App() {
             <div className="flex flex-col sm:flex-row gap-4 sm:gap-28 mt-12 w-[80%]">
               <div>
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
-                  Git
+                REACTJS
                 </p>
               </div>
               <div>
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
-                  Wordpress
+                NEXTJS
                 </p>
               </div>
               <div>
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
-                  Teamwork
+                JAVASCRIPT
                 </p>
               </div>
               <div>
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
-                  Quick learning
+                TYPESCRIPT
+                </p>
+              </div>
+              <div>
+                <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
+                HTML5
+                </p>
+              </div>
+              <div>
+                <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
+                CSS3
                 </p>
               </div>
             </div>
             <div className="flex flex-col sm:flex-row gap-4 sm:gap-36 mt-4 sm:mt-6 w-[80%]">
+            
+             
               <div>
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
-                  Engagement
+                JQUERY
                 </p>
               </div>
               <div>
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
-                  B2 english
+                NODEJS
                 </p>
               </div>
               <div>
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
-                  RWD
+                REDUX
+                </p>
+              </div>
+              <div>
+                <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
+                MONGODB
+                </p>
+              </div>
+              <div>
+                <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
+                GIT
                 </p>
               </div>
             </div>
           </div>
         </section>
-        <section className="py-8" id="aboutme">
+        {/* <section className="py-8" id="aboutme">
           <div className="container m-auto px-4">
             <h2 className="text-2xl font-semibold">About me</h2>
             <div className="mt-12 relative before:absolute before:top-0 before:left-16 before:rounded-full before:bottom-10 sm:before:bottom-2 before:w-1 before:bg-white">
               <div className="pl-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
-                <h3 className="absolute left-0 text-lg font-semibold">2015</h3>
+                <h3 className="absolute left-0 text-lg font-semibold">2021</h3>
                 <p>
-                  Graduated KTC (Krasnodar Technical College) and for about two
-                  years worked with high voltage (35, 110kV) substations as the
-                  specialty electrician of emergency mobile crew. Later
-                  qualified as a Crossfit trainer.
+                  Graduated Ghousia College Of Engineering 
                 </p>
               </div>
               <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
-                <h3 className="absolute left-0 text-lg font-semibold">2015</h3>
+                <h3 className="absolute left-0 text-lg font-semibold">2022-2023</h3>
                 <p>
-                  Underwent a digital marketing course and started to work as a
-                  PPC and project manager in a local web studio. After 3 months
-                  started my own freelance practice.
+                 I joined Liquid Lab as a frontend developer. I am thrilled to be a part of this dynamic team, where I can apply and enhance my skills in web development. 
                 </p>
               </div>
               <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
-                <h3 className="absolute left-0 text-lg font-semibold">2015</h3>
+                <h3 className="absolute left-0 text-lg font-semibold">2023-2024</h3>
                 <p>
-                  Became more interested in IT and decided to try programming.
-                  Underwent Java course and made a project.
+                 Now Currently Working as Front-end Developer @ Nexevo Technology.
                 </p>
               </div>
-              <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
-                <h3 className="absolute left-0 text-lg font-semibold">2015</h3>
-                <p>
-                  Worked out in which field I want to build a developer's career
-                  and started to learn Frontend Development.
-                </p>
-              </div>
+             
             </div>
           </div>
-        </section>
+        </section> */}
       </main>
       <footer>
         <div className="container m-auto flex justify-between px-4 py-6">
