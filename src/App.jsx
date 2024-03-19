1301d978598829c39553d022c4b6de10c5f4769b
vikas <vikasrb24@gmail.com>
2024-03-19 12:54:41 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -242,7 +242,7 @@ function App() {
               <div className="border border-gray-500 rounded-md p-5 flex-1">
                 <img src={project5} className="w-full h-auto" />
                 <h3 className="text-2xl font-semibold mt-8">
-                 GPT_3
+                  Zomoto Clone
                 </h3>
                 <p className="text-gray-400 text-sm mt-2">
                 Responsive React application for an online Zomoto Clone. Built with HTML5, CSS3 (SCSS), and React.
@@ -256,23 +256,7 @@ function App() {
                   </button>
                 </div>
               </div>
-              <div className="border border-gray-500 rounded-md p-5 flex-1">
-                <img src={Project4} className="w-full h-auto" />
-                <h3 className="text-2xl font-semibold mt-8">
-                Restaurant Website
-                </h3>
-                <p className="text-gray-400 text-sm mt-2">
-                Responsive React application for an online store of Restaurant Website. Built with HTML5, CSS3 (SCSS), and React.
-                </p>
-                <div className="flex gap-2 mt-12">
-                  <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700"  onClick={openLiveProjectresWeb}>
-                    Live preview
-                  </button>
-                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500" onClick={openGithubRepositoryresWeb}>
-                    Checkout github
-                  </button>
-                </div>
-              </div>
+              
             </div>
           </div>
         </section>
