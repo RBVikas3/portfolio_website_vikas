948e7d1e1872ef60648842345b7ebd5ac24e1cee
vikasrb <vikasrb@sunplussoftware.com>
2025-09-07 16:01:46 +0530

    add some changes in newfile

--- src/NewFile.jsx
+++ src/NewFile.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const NewFile = () => {
   return (
-    <div>NewFile</div>
+    <div>
+                <p className="mt-4 text-gray-400">
+                Highly motivated Front-end developer 1.2 years with a passion for crafting engaging UIs and backend architecture. Built  30 websites (personal + professional), proficient in React, Next.js, MongoDB, Node.js, jQuery, AWS, payment integrations (RezorPay, Stripe). </p>
+                <button className="px-8 shadow-gray-500 shadow-md py-5 mt-5 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                  Download resume
+                </button>
+              </div>
   )
 }
 
